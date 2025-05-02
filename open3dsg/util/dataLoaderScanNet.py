3e2fa129e830f29a26b52a57da1fdbd647f94cec
FCQ <19316344691@163.com>
2025-05-03 01:43:33 +0800

    添加load_scannet函数注释

--- open3dsg/util/dataLoaderScanNet.py
+++ open3dsg/util/dataLoaderScanNet.py
@@ -68,33 +68,37 @@ def scannet_get_instance_ply(plydata, segs, aggre, random_color=False):
 
 def load_scannet(pth_ply, pth_agg, pth_seg, verbose=False, random_color=False):
     ''' Load GT '''
-    plydata = trimesh.load(pth_ply, process=False)
-    num_verts = plydata.vertices.shape[0]
+    plydata = trimesh.load(pth_ply, process=False) # pth_ply: 比如scene0191_00_vh_clean_2.labels.ply 点云数据
+    num_verts = plydata.vertices.shape[0]  # 顶点的个数 
     if verbose:
         print('num of verts:', num_verts)
 
     ''' Load segment file'''
-    with open(pth_seg) as f:
+    with open(pth_seg) as f: # pth_seg: 比如scene0191_00_vh_clean_2.0.010000.segs.json 分割数据 
         segs = json.load(f)
     if verbose:
         print('len(aggre[\'segIndices\']):', len(segs['segIndices']))
-    segment_ids = list(np.unique(np.array(segs['segIndices'])))  # get unique segment ids
+    segment_ids = list(np.unique(np.array(segs['segIndices'])))  # 分割ID 每个分割ID包含多个顶点
     if verbose:
         print('num of unique ids:', len(segment_ids))
 
     ''' Load aggregation file'''
-    with open(pth_agg) as f:
+    with open(pth_agg) as f: # pth_agg: 比如scene0191_00.aggregation.json 聚合数据
         aggre = json.load(f)
     # assert(aggre['sceneId'].split('scannet.')[1]==scan_id)
     # assert(aggre['segmentsFile'].split('scannet.')[1] == scan_id+args.segs)
 
     plydata, instances = scannet_get_instance_ply(plydata, segs, aggre, random_color=random_color)
+    # instances 每个顶点的实例ID (145736,) 指的是聚合文件中的segGroup的id
 
-    labels = plydata.metadata['_ply_raw']['vertex']['data']['label'].flatten()
-    points = plydata.vertices
+    labels = plydata.metadata['_ply_raw']['vertex']['data']['label'].flatten() # (145736,) 点云数据中原本携带的
+    points = plydata.vertices # (145736, 3) 每一行为顶点的xyz坐标
 
     # the label is in the range of 1 to 40. 0 is unlabeled
     # instance 0 is unlabeled.
+    # labels vs instances 
+    # labels 存储的是语义分割信息，它表示每个点所属的物体类别,例如：所有椅子的点都会被标记为相同的类别ID（如例如ID 7），即使是不同的椅子
+    # instances 存储的是实例分割信息，它表示每个点所属的具体物体实例，例如：房间中的两把不同椅子会有不同的实例ID（如实例ID 3和实例ID 8）
     return plydata, points, labels, instances
 
 
